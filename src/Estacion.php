0423d82d77dd5b4f925498aff70688d86b5fecc9
Ciro Teves <32022610+LordRegginald@users.noreply.github.com>
2017-09-27 14:25:07 -0300

    #17 Añadidas las funciones sacarBici() y dejarBici().

--- src/Estacion.php
+++ src/Estacion.php
@@ -2,25 +2,36 @@
 
 namespace Mibici;
 
-class Estacion implements EstacionInterface{
+class Estacion implements EstacionInterface {
 
 	protected $anclajeLibres;
 	protected $anclajeTotales;
+	protected $bicicletas;
 
-	public function _construct($a,$b){
+	public function _construct($a,$b) {
+		$this->bicicletas=[]
 		$this->anclajeLibres=$a;
 		$this->anclajeTotales=$b;
 	}
+	
+	public funtion sacarBici(BicicletaInterface $bici) {
+		$clave = array_search($bici, $bicicletas);
+		unset($bicicletas[$clave]);
+	}
+	
+	public function dejarBici(BicicletaInterface $bici) {
+		array_push($bici,$bicicletas);
+	}
 
-	public function anclajesTotales(){
+	public function anclajesTotales() {
 		return $this->anclajeTotales;
 	}
 
-	public function anclajesDisponibles(){
+	public function anclajesDisponibles() {
 		return $this->anclajeLibres;
 	}
 
-	public function bicicletasDisponibles(){
+	public function bicicletasDisponibles() {
 		return ($this->anclajeTotales - $this->anclajeLibres);
 	}
 
